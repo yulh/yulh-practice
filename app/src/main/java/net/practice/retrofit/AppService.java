5a32f96b761b89ef895817e336fce76a0f0e6a48
yulh <2919498196@qq.com>
2018-10-16 17:56:47 +0800

    删除retrofit library

---
+++ app/src/main/java/net/practice/retrofit/AppService.java
@@ -0,0 +1,17 @@
+package net.practice.retrofit;
+
+import com.retrofit.ApiStores;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+
+public class AppService {
+    /**
+     * 下载最新模板图片
+     *
+     * @param api
+     */
+    public static Call<ResponseBody> downloadLatestFeature(ApiStores api, String imageUrl) {
+        return api.downloadLatestFeature(imageUrl);
+    }
+}
---
+++ app/src/main/java/net/practice/utlis/DownloadImageUtils.java
@@ -0,0 +1,79 @@
+package net.practice.utlis;
+
+import com.retrofit.ApiStores;
+import com.retrofit.AppService;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DownloadImageUtils {
+
+    public final static String APP_IMAGE_DIR = "";
+
+    /**
+     * 下载图片到SD卡
+     *
+     * @param mApi
+     * @param url
+     * @param imageName
+     */
+    public static void downloadLatestFeature(ApiStores mApi, final String url, final String imageName) {
+        Call<ResponseBody> resultCall = AppService.downloadLatestFeature(mApi, url);
+        resultCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                writeResponseBodyToDisk(imageName, response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            }
+        });
+    }
+
+    /**
+     * 保存下载的图片流写入SD卡文件
+     *
+     * @param imageName xxx.jpg
+     * @param body      image stream
+     */
+    public static void writeResponseBodyToDisk(String imageName, ResponseBody body) {
+        if (body == null) {
+            //ToastUtils.showToast("图片源错误");
+            return;
+        }
+        try {
+            InputStream is = body.byteStream();
+            File fileDr = new File(APP_IMAGE_DIR);
+            if (!fileDr.exists()) {
+                fileDr.mkdir();
+            }
+            File file = new File(APP_IMAGE_DIR, imageName);
+            if (file.exists()) {
+                file.delete();
+                file = new File(APP_IMAGE_DIR, imageName);
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(is);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = bis.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            fos.flush();
+            fos.close();
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
